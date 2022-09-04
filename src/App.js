f5f3e84c31b6a5eb931c93d4e78e1db2249e6d19
tetloose <tetloose@gmail.com>
2022-09-04 18:08:10 +0100

    moved location + weather into components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './scss/location/location.styles.scss'
-import './scss/weather/weather.styles.scss'
+import Location from './components/location/location.component'
+import Weather from './components/weather/weather.component'
 
 const App = () => {
   return (
@@ -7,115 +7,12 @@ const App = () => {
       <section className="l-container__block">
         <div className="l-row">
           <div className="l-row__col is-centered is-med-half">
-            <input
-              type="search"
-              id="location-id"
-              name="location-name"
-              placeholder="Weather by location"
-              className="location"
-            />
+            <Location />
           </div>
         </div>
         <div className="l-row is-med-justify-content-evenly has-med-small-gutter">
           <div className="l-row__col">
-            <div className="weather weather--shower-rain">
-              <time className='weather__date'>
-                Today 15th
-              </time>
-              <div className="weather__details">
-                <div className="weather__icon weather__icon--static" />
-                <div className="weather__description">
-                  Shower Rain
-                </div>
-                <div className="weather__temp weather__temp--celsius">
-                  22&#8451;
-                </div>
-                <div className="weather__temp weather__temp--fahrenheit">
-                  72&#8457;
-                </div>
-              </div>
-              <div className="weather__icon weather__icon--fixed" />
-            </div>
-          </div>
-          <div className="l-row__col">
-            <div className="weather weather--rain">
-              <time className='weather__date'>
-                Today 15th
-              </time>
-              <div className="weather__details">
-                <div className="weather__icon weather__icon--static" />
-                <div className="weather__description">
-                  Rain
-                </div>
-                <div className="weather__temp weather__temp--celsius">
-                  22&#8451;
-                </div>
-                <div className="weather__temp weather__temp--fahrenheit">
-                  72&#8457;
-                </div>
-              </div>
-              <div className="weather__icon weather__icon--fixed" />
-            </div>
-          </div>
-          <div className="l-row__col">
-            <div className="weather weather--snow">
-              <time className='weather__date'>
-                Today 15th
-              </time>
-              <div className="weather__details">
-                <div className="weather__icon weather__icon--static" />
-                <div className="weather__description">
-                  Snow
-                </div>
-                <div className="weather__temp weather__temp--celsius">
-                  22&#8451;
-                </div>
-                <div className="weather__temp weather__temp--fahrenheit">
-                  72&#8457;
-                </div>
-              </div>
-              <div className="weather__icon weather__icon--fixed" />
-            </div>
-          </div>
-          <div className="l-row__col">
-            <div className="weather weather--thunderstorm">
-              <time className='weather__date'>
-                Today 15th
-              </time>
-              <div className="weather__details">
-                <div className="weather__icon weather__icon--static" />
-                <div className="weather__description">
-                  Thunderstorm
-                </div>
-                <div className="weather__temp weather__temp--celsius">
-                  22&#8451;
-                </div>
-                <div className="weather__temp weather__temp--fahrenheit">
-                  72&#8457;
-                </div>
-              </div>
-              <div className="weather__icon weather__icon--fixed" />
-            </div>
-          </div>
-          <div className="l-row__col">
-            <div className="weather weather--clear-sky">
-              <time className='weather__date'>
-                Today 15th
-              </time>
-              <div className="weather__details">
-                <div className="weather__icon weather__icon--static" />
-                <div className="weather__description">
-                  Clear sky
-                </div>
-                <div className="weather__temp weather__temp--celsius">
-                  22&#8451;
-                </div>
-                <div className="weather__temp weather__temp--fahrenheit">
-                  72&#8457;
-                </div>
-              </div>
-              <div className="weather__icon weather__icon--fixed" />
-            </div>
+            <Weather />
           </div>
         </div>
       </section>
---
+++ src/components/location/location.component.jsx
@@ -0,0 +1,15 @@
+import './location.styles.scss'
+
+const Location = () => {
+  return (
+    <input
+      type="search"
+      id="location-id"
+      name="location-name"
+      placeholder="Weather by location"
+      className="location"
+    />
+  )
+}
+
+export default Location;
