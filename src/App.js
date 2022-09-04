6aa427149de4415010bd829aad8dbf9beb6a19b8
tetloose <tetloose@gmail.com>
2022-09-04 19:06:53 +0100

    add state for location, add react-debounce-input to location search, add props to location

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import Location from './components/location/location.component'
+import { useState, useEffect } from 'react';
+
+import SearchField from './components/search-field/search-field.component'
 import Weather from './components/weather/weather.component'
 
 const App = () => {
+  const [locationField, setLocationField] = useState('London')
+
+  const onLocationChange = (event) => {
+    const locationFieldString = event.target.value.toLocaleLowerCase()
+
+    setLocationField(locationFieldString)
+  }
+
+  useEffect(() => {
+    console.log('state updated', locationField)
+  }, [locationField] )
+
   return (
     <main className="l-container is-align-item-center">
       <section className="l-container__block">
         <div className="l-row">
           <div className="l-row__col is-centered is-med-half">
-            <Location />
+            <SearchField
+              debounceTimer={500}
+              className='location'
+              value={locationField}
+              placeholder='Enter Location GB'
+              onChangeHandler={onLocationChange}
+            />
           </div>
         </div>
         <div className="l-row is-med-justify-content-evenly has-med-small-gutter">
